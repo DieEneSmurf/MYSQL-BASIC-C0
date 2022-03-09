0bfeb656d6c99cb97194314e649a2a8f2bde9f66
Daanmorel <2152772@talnet.nl>
2022-03-09 09:47:59 +0100

    Opdrachten gemaakt (Nog niet alles)

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -1,15 +1,15 @@
 -- Opdracht 1 
-
+SELECT * FROM jaar2015 WHERE rank = 7;
 -- Opdracht 2 
-
+SELECT * FROM jaar2016 WHERE country = "Netherlands";
 -- Opdracht 3
-
+SELECT * FROM jaar2015 WHERE country = "Netherlands";
 -- Opdracht 4
-
+SELECT * FROM jaar2016 WHERE country = "Netherlands";
 -- Opdracht 5
-
+SELECT * FROM jaar2016 WHERE region = "Western Europe"
 -- Opdracht 6 
-
+-- het is woensdag en ik wil best deze dingen doen alleen ik ben in mn eentje op school en er is niemand die me kan helpen. ik rond dit later af
 -- Opdracht 7 
 
 -- Opdracht 8
