f31446c4bda1bb1c760176ed03b8dffab8c99a30
Daanmorel <2152772@talnet.nl>
2022-03-11 11:01:24 +0100

    Antwoorden SELECT - WHERE

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -7,13 +7,14 @@ SELECT * FROM jaar2015 WHERE country = "Netherlands";
 -- Opdracht 4
 SELECT * FROM jaar2016 WHERE country = "Netherlands";
 -- Opdracht 5
-SELECT * FROM jaar2016 WHERE region = "Western Europe"
+SELECT * FROM jaar2016 WHERE region = "North-america"
 -- Opdracht 6 
--- het is woensdag en ik wil best deze dingen doen alleen ik ben in mn eentje op school en er is niemand die me kan helpen. ik rond dit later af
+SELECT country FROM jaar2016 WHERE rank="25";
 -- Opdracht 7 
-
+SELECT country FROM jaar2016 WHERE score>"7087"
 -- Opdracht 8
-
+SELECT country, region FROM jaar2016 WHERE rank<="5"
 -- Opdracht 9
-
+SELECT score FROM jaar2015 WHERE country="Ireland"
 -- Opdracht 10
+SELECT country FROM jaar2016 WHERE region="Latin America and Caribbean"
